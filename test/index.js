1112fccb944b5ee73b051e0a8ccd824aebf2169b
Matt Simerson <matt@tnpi.net>
2024-04-17 19:56:22 -0700

    Release v1.2.5 (#22)
    - get_mx: filter out implicit MX records
    - dep: eslint-plugin-haraka -> @haraka/eslint-config
    - chore: lint: remove duplicate / stale rules from .eslintrc
    - chore: populate [files] in package.json. Delete .npmignore.
    - doc(CONTRIBUTORS): added
    - doc(CHANGES): renamed CHANGELOG
    - chore: prettier & lint

--- test/index.js
+++ test/index.js
@@ -1,26 +1,25 @@
-
 // node.js built-in modules
-const assert   = require('assert')
+const assert = require('assert')
 
 // npm modules
-const Address      = require('address-rfc2821').Address;
-const constants    = require('haraka-constants');
-const fixtures     = require('haraka-test-fixtures')
+const Address = require('address-rfc2821').Address
+const constants = require('haraka-constants')
+const fixtures = require('haraka-test-fixtures')
 
-const SPF = require('../lib/spf').SPF;
-const spf = new SPF();
+const SPF = require('../lib/spf').SPF
+const spf = new SPF()
 
 beforeEach(function () {
   this.plugin = new fixtures.plugin('spf')
 
-  this.plugin.timeout = 8000;
-  this.plugin.load_spf_ini();
+  this.plugin.timeout = 8000
+  this.plugin.load_spf_ini()
 
   // comment this line to see detailed SPF evaluation
-  this.plugin.SPF.prototype.log_debug = () => {};
+  this.plugin.SPF.prototype.log_debug = () => {}
 
-  this.connection = fixtures.connection.createConnection();
-  this.connection.transaction = fixtures.transaction.createTransaction();
+  this.connection = fixtures.connection.createConnection()
+  this.connection.init_transaction()
 })
 
 describe('spf', function () {
@@ -38,216 +37,336 @@ describe('load_spf_ini', function () {
 
 describe('return_results', function () {
   it('result, none, reject=false', function (done) {
-    this.plugin.cfg.deny.mfrom_none=false;
-    this.plugin.return_results(function next () {
-      assert.equal(undefined, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_NONE, 'test@example.com');
+    this.plugin.cfg.deny.mfrom_none = false
+    this.plugin.return_results(
+      function next() {
+        assert.equal(undefined, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_NONE,
+      'test@example.com',
+    )
   })
 
   it('result, none, reject=true', function (done) {
-
-    this.plugin.cfg.deny.mfrom_none=true;
-    this.plugin.return_results(function next () {
-      assert.equal(DENY, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_NONE, 'test@example.com');
+    this.plugin.cfg.deny.mfrom_none = true
+    this.plugin.return_results(
+      function next() {
+        assert.equal(DENY, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_NONE,
+      'test@example.com',
+    )
   })
 
   it('result, neutral', function (done) {
-    this.plugin.return_results(function next () {
-      assert.equal(undefined, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_NEUTRAL, 'test@example.com');
+    this.plugin.return_results(
+      function next() {
+        assert.equal(undefined, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_NEUTRAL,
+      'test@example.com',
+    )
   })
 
   it('result, pass', function (done) {
-    this.plugin.return_results(function next () {
-      assert.equal(undefined, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_PASS, 'test@example.com');
+    this.plugin.return_results(
+      function next() {
+        assert.equal(undefined, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_PASS,
+      'test@example.com',
+    )
   })
 
   it('result, softfail, reject=false', function (done) {
-    this.plugin.cfg.deny.mfrom_softfail=false;
-    this.plugin.return_results(function next () {
-      assert.equal(undefined, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_SOFTFAIL, 'test@example.com');
+    this.plugin.cfg.deny.mfrom_softfail = false
+    this.plugin.return_results(
+      function next() {
+        assert.equal(undefined, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_SOFTFAIL,
+      'test@example.com',
+    )
   })
 
   it('result, softfail, reject=true', function (done) {
-    this.plugin.cfg.deny.mfrom_softfail=true;
-    this.plugin.return_results(function next () {
-      assert.equal(DENY, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_SOFTFAIL, 'test@example.com');
+    this.plugin.cfg.deny.mfrom_softfail = true
+    this.plugin.return_results(
+      function next() {
+        assert.equal(DENY, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_SOFTFAIL,
+      'test@example.com',
+    )
   })
 
   it('result, fail, reject=false', function (done) {
-    this.plugin.cfg.deny.mfrom_fail=false;
-    this.plugin.return_results(function next () {
-      assert.equal(undefined, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_FAIL, 'test@example.com');
+    this.plugin.cfg.deny.mfrom_fail = false
+    this.plugin.return_results(
+      function next() {
+        assert.equal(undefined, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_FAIL,
+      'test@example.com',
+    )
   })
 
   it('result, fail, reject=true', function (done) {
-    this.plugin.cfg.deny.mfrom_fail=true;
-    this.plugin.return_results(function next () {
-      assert.equal(DENY, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_FAIL, 'test@example.com');
+    this.plugin.cfg.deny.mfrom_fail = true
+    this.plugin.return_results(
+      function next() {
+        assert.equal(DENY, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_FAIL,
+      'test@example.com',
+    )
   })
 
   it('result, temperror, reject=false', function (done) {
-    this.plugin.cfg.defer.mfrom_temperror=false;
-    this.plugin.return_results(function next () {
-      assert.equal(undefined, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_TEMPERROR, 'test@example.com');
+    this.plugin.cfg.defer.mfrom_temperror = false
+    this.plugin.return_results(
+      function next() {
+        assert.equal(undefined, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_TEMPERROR,
+      'test@example.com',
+    )
   })
 
   it('result, temperror, reject=true', function (done) {
-    this.plugin.cfg.defer.mfrom_temperror=true;
-    this.plugin.return_results(function next () {
-      assert.equal(DENYSOFT, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_TEMPERROR, 'test@example.com');
+    this.plugin.cfg.defer.mfrom_temperror = true
+    this.plugin.return_results(
+      function next() {
+        assert.equal(DENYSOFT, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_TEMPERROR,
+      'test@example.com',
+    )
   })
 
   it('result, permerror, reject=false', function (done) {
-    this.plugin.cfg.deny.mfrom_permerror=false;
-    this.plugin.return_results(function next () {
-      assert.equal(undefined, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_PERMERROR, 'test@example.com');
+    this.plugin.cfg.deny.mfrom_permerror = false
+    this.plugin.return_results(
+      function next() {
+        assert.equal(undefined, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_PERMERROR,
+      'test@example.com',
+    )
   })
 
   it('result, permerror, reject=true', function (done) {
-    this.plugin.cfg.deny.mfrom_permerror=true;
-    this.plugin.return_results(    function next () {
-      assert.equal(DENY, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', spf.SPF_PERMERROR, 'test@example.com');
+    this.plugin.cfg.deny.mfrom_permerror = true
+    this.plugin.return_results(
+      function next() {
+        assert.equal(DENY, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      spf.SPF_PERMERROR,
+      'test@example.com',
+    )
   })
 
   it('result, unknown', function (done) {
-    this.plugin.return_results(function next () {
-      assert.equal(undefined, arguments[0]);
-      done()
-    }, this.connection, spf, 'mfrom', 'unknown', 'test@example.com');
+    this.plugin.return_results(
+      function next() {
+        assert.equal(undefined, arguments[0])
+        done()
+      },
+      this.connection,
+      spf,
+      'mfrom',
+      'unknown',
+      'test@example.com',
+    )
   })
 })
 
 describe('hook_helo', function () {
   it('rfc1918', function (done) {
-    let completed = 0;
-    function next (rc) {
-      completed++;
-      assert.equal(undefined, rc);
+    let completed = 0
+    function next(rc) {
+      completed++
+      assert.equal(undefined, rc)
       if (completed >= 2) done()
     }
-    this.connection.remote.is_private=true;
-    this.plugin.helo_spf(next, this.connection);
-    this.plugin.helo_spf(next, this.connection, 'helo.sender.com');
+    this.connection.remote.is_private = true
+    this.plugin.helo_spf(next, this.connection)
+    this.plugin.helo_spf(next, this.connection, 'helo.sender.com')
   })
 
   it('IPv4 literal', function (done) {
-    this.connection.remote.ip='190.168.1.1';
-    this.plugin.helo_spf(function next (rc) {
-      assert.equal(undefined, rc);
-      done()
-    }, this.connection, '[190.168.1.1]' );
+    this.connection.remote.ip = '190.168.1.1'
+    this.plugin.helo_spf(
+      function next(rc) {
+        assert.equal(undefined, rc)
+        done()
+      },
+      this.connection,
+      '[190.168.1.1]',
+    )
   })
 
   it('MX with no A record', function (done) {
     this.timeout(5000)
-    this.connection.set('remote.ip', '192.0.2.0');
-    this.plugin.helo_spf(function next (rc) {
-      assert.equal(undefined, rc);
-      done()
-    }, this.connection, 'haraka-test.tnpi.net' );
+    this.connection.set('remote.ip', '192.0.2.0')
+    this.plugin.helo_spf(
+      function next(rc) {
+        assert.equal(undefined, rc)
+        done()
+      },
+      this.connection,
+      'test.haraka.tnpi.net',
+    )
   })
 })
 
-const test_addr = new Address('<test@example.com>');
+const test_addr = new Address('<test@example.com>')
 
 describe('hook_mail', function () {
+  this.timeout(5000)
 
   it('rfc1918', function (done) {
-    this.connection.set('remote.is_private', true);
-    this.connection.set('remote.ip', '192.168.1.1');
-    this.plugin.hook_mail(function next () {
-      assert.equal(undefined, arguments[0]);
-      done()
-    }, this.connection, [test_addr]);
+    this.connection.set('remote.is_private', true)
+    this.connection.set('remote.ip', '192.168.1.1')
+    this.plugin.hook_mail(
+      function next() {
+        assert.equal(undefined, arguments[0])
+        done()
+      },
+      this.connection,
+      [test_addr],
+    )
   })
 
   it('rfc1918 relaying', function (done) {
-    this.connection.set('remote.is_private', true);
-    this.connection.set('remote.ip','192.168.1.1');
-    this.connection.relaying=true;
-    this.plugin.hook_mail(function next () {
-      assert.ok([undefined, constants.CONT].includes(arguments[0]));
-      done()
-    }, this.connection, [test_addr]);
+    this.connection.set('remote.is_private', true)
+    this.connection.set('remote.ip', '192.168.1.1')
+    this.connection.relaying = true
+    this.plugin.hook_mail(
+      function next() {
+        assert.ok([undefined, constants.CONT].includes(arguments[0]))
+        done()
+      },
+      this.connection,
+      [test_addr],
+    )
   })
 
   it('no txn', function (done) {
-    this.connection.remote.ip='207.85.1.1';
-    delete this.connection.transaction;
-    this.plugin.hook_mail(function next () {
-      assert.equal(undefined, arguments[0]);
-      assert.equal(undefined, arguments[1]);
+    this.connection.remote.ip = '207.85.1.1'
+    delete this.connection.transaction
+    this.plugin.hook_mail(function next() {
+      assert.equal(undefined, arguments[0])
+      assert.equal(undefined, arguments[1])
       done()
-    }, this.connection);
+    }, this.connection)
   })
 
   it('txn, no helo', function (done) {
-    this.timeout(3000)
-    this.plugin.cfg.deny.mfrom_fail = false;
-    this.connection.set('remote.ip', '207.85.1.1');
-    this.plugin.hook_mail(function next () {
-      assert.equal(undefined, arguments[0]);
-      assert.equal(undefined, arguments[1]);
-      done()
-    }, this.connection, [test_addr]);
+    this.plugin.cfg.deny.mfrom_fail = false
+    this.connection.set('remote.ip', '207.85.1.1')
+    this.plugin.hook_mail(
+      function next() {
+        assert.equal(undefined, arguments[0])
+        assert.equal(undefined, arguments[1])
+        done()
+      },
+      this.connection,
+      [test_addr],
+    )
   })
 
   it('txn', function (done) {
-    this.timeout(3000)
-    this.connection.set('remote.ip', '207.85.1.1');
-    this.connection.set('hello.host', 'mail.example.com');
-    this.plugin.hook_mail(function next (rc) {
-      assert.equal(undefined, rc);
-      done()
-    }, this.connection, [test_addr]);
+    this.connection.set('remote.ip', '207.85.1.1')
+    this.connection.set('hello.host', 'mail.example.com')
+    this.plugin.hook_mail(
+      function next(rc) {
+        assert.equal(undefined, rc)
+        done()
+      },
+      this.connection,
+      [test_addr],
+    )
   })
 
   it('txn, relaying', function (done) {
-    this.timeout(3000)
-    this.connection.set('remote.ip', '207.85.1.1');
-    this.connection.set('relaying', true);
-    this.connection.set('hello.host', 'mail.example.com');
-    this.plugin.hook_mail(function next (rc) {
-      assert.equal(undefined, rc);
-      done()
-    }, this.connection, [test_addr]);
+    this.connection.set('remote.ip', '207.85.1.1')
+    this.connection.set('relaying', true)
+    this.connection.set('hello.host', 'mail.example.com')
+    this.plugin.hook_mail(
+      function next(rc) {
+        assert.equal(undefined, rc)
+        done()
+      },
+      this.connection,
+      [test_addr],
+    )
   })
 
   it('txn, relaying, is_private', function (done) {
-    this.timeout(8000)
-    this.plugin.cfg.relay.context='myself';
-    this.plugin.cfg.deny_relay.mfrom_fail = true;
-    this.connection.set('remote.ip', '127.0.1.1');
-    this.connection.set('remote.is_private', true);
-    this.connection.relaying = true;
-    this.connection.set('hello.host', 'www.tnpi.net');
-    this.plugin.nu.public_ip = '66.128.51.165';
-    this.plugin.hook_mail(function next (rc) {
-      assert.equal(undefined, rc);
-      done()
-    }, this.connection, [new Address('<nonexist@tnpi.net>')]);
+    this.timeout(12000)
+    this.plugin.cfg.relay.context = 'myself'
+    this.plugin.cfg.deny_relay.mfrom_fail = true
+    this.connection.set('remote.ip', '127.0.1.1')
+    this.connection.set('remote.is_private', true)
+    this.connection.relaying = true
+    this.connection.set('hello.host', 'www.tnpi.net')
+    this.plugin.nu.public_ip = '66.128.51.165'
+    this.plugin.hook_mail(
+      function next(rc) {
+        assert.equal(undefined, rc)
+        done()
+      },
+      this.connection,
+      [new Address('<nonexist@tnpi.net>')],
+    )
   })
 })
--- test/spf.js
+++ test/spf.js
@@ -1,10 +1,8 @@
 const assert = require('assert')
 
-const SPF = require('../lib/spf').SPF;
+const SPF = require('../lib/spf').SPF
 
-SPF.prototype.log_debug = () => {};  // noop, hush debug output
-
-
+SPF.prototype.log_debug = () => {} // noop, hush debug output
 
 beforeEach(function () {
   this.SPF = new SPF()
@@ -12,32 +10,32 @@ beforeEach(function () {
 
 describe('SPF', function () {
   it('new SPF', function () {
-    assert.ok(this.SPF);
+    assert.ok(this.SPF)
   })
 
   it('constants', function () {
-    assert.equal(1, this.SPF.SPF_NONE);
-    assert.equal(2, this.SPF.SPF_PASS);
-    assert.equal(3, this.SPF.SPF_FAIL);
-    assert.equal(4, this.SPF.SPF_SOFTFAIL);
-    assert.equal(5, this.SPF.SPF_NEUTRAL);
-    assert.equal(6, this.SPF.SPF_TEMPERROR);
-    assert.equal(7, this.SPF.SPF_PERMERROR);
-    assert.equal(10, this.SPF.LIMIT);
+    assert.equal(1, this.SPF.SPF_NONE)
+    assert.equal(2, this.SPF.SPF_PASS)
+    assert.equal(3, this.SPF.SPF_FAIL)
+    assert.equal(4, this.SPF.SPF_SOFTFAIL)
+    assert.equal(5, this.SPF.SPF_NEUTRAL)
+    assert.equal(6, this.SPF.SPF_TEMPERROR)
+    assert.equal(7, this.SPF.SPF_PERMERROR)
+    assert.equal(10, this.SPF.LIMIT)
   })
 
   it('mod_redirect, true', async function () {
-    this.SPF.been_there['example.com'] = true;
+    this.SPF.been_there['example.com'] = true
     const rc = await this.SPF.mod_redirect('example.com')
     // assert.equal(null, err);
-    assert.equal(1, rc);
+    assert.equal(1, rc)
   })
 
   it('mod_redirect, false', async function () {
-    this.timeout=4000
-    this.SPF.count=0;
-    this.SPF.ip='212.70.129.94';
-    this.SPF.mail_from='fraud@aexp.com';
+    this.timeout = 4000
+    this.SPF.count = 0
+    this.SPF.ip = '212.70.129.94'
+    this.SPF.mail_from = 'fraud@aexp.com'
 
     const rc = await this.SPF.mod_redirect('aexp.com')
     switch (rc) {
@@ -45,53 +43,59 @@ describe('SPF', function () {
         // from time to time (this is the third time we've seen it,
         // American Express publishes an invalid SPF record which results
         // in a PERMERROR. Ignore it.
-        assert.equal(rc, 7, "aexp SPF record is broken again");
-        break;
+        assert.equal(rc, 7, 'aexp SPF record is broken again')
+        break
       case 6:
-        assert.equal(rc, 6, "temporary (likely DNS timeout) error");
-        break;
+        assert.equal(rc, 6, 'temporary (likely DNS timeout) error')
+        break
       default:
-        assert.equal(rc, 3);
+        assert.equal(rc, 3)
     }
   })
 
   it('check_host, gmail.com, fail', async function () {
-    this.timeout = 3000;
-    this.SPF.count=0;
-    const rc = await this.SPF.check_host('212.70.129.94', 'gmail.com', 'haraka.mail@gmail.com')
+    this.timeout = 3000
+    this.SPF.count = 0
+    const rc = await this.SPF.check_host(
+      '212.70.129.94',
+      'gmail.com',
+      'haraka.mail@gmail.com',
+    )
     switch (rc) {
       case 1:
-        assert.equal(rc, 1, "none");
-        console.log('Why do DNS lookup fail to find gmail SPF record on GitHub Actions?');
-        break;
+        assert.equal(rc, 1, 'none')
+        console.log(
+          'Why do DNS lookup fail to find gmail SPF record on GitHub Actions?',
+        )
+        break
       case 3:
-        assert.equal(rc, 3, "fail");
-        break;
+        assert.equal(rc, 3, 'fail')
+        break
       case 4:
-        assert.equal(rc, 4, "soft fail");
-        break;
+        assert.equal(rc, 4, 'soft fail')
+        break
       case 7:
-        assert.equal(rc, 7, "perm error");
-        break;
+        assert.equal(rc, 7, 'perm error')
+        break
       default:
         assert.equal(rc, 4)
     }
   })
 
   it('check_host, facebook.com, pass', async function () {
-    this.timeout = 3000;
-    this.SPF.count = 0;
-    const rc = await this.SPF.check_host('69.171.232.145', 'facebookmail.com');
-    assert.equal(rc, this.SPF.SPF_PASS, "pass");
+    this.timeout = 3000
+    this.SPF.count = 0
+    const rc = await this.SPF.check_host('69.171.232.145', 'facebookmail.com')
+    assert.equal(rc, this.SPF.SPF_PASS, 'pass')
   })
 
   it('valid_ip, true', function (done) {
-    assert.equal(this.SPF.valid_ip(':212.70.129.94'), true);
+    assert.equal(this.SPF.valid_ip(':212.70.129.94'), true)
     done()
   })
 
   it('valid_ip, false', function (done) {
-    assert.equal(this.SPF.valid_ip(':212.70.d.94'), false);
+    assert.equal(this.SPF.valid_ip(':212.70.d.94'), false)
     done()
   })
 })
